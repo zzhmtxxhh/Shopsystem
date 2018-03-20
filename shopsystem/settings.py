cab74cf890946530edb82f206f7ba4b861c8ce22
Walter zhang <walterzhang@Walters-MacBook-Pro.local>
2018-03-20 14:50:54 -0400

    Finished the configuration of Mysql

--- shopsystem/settings.py
+++ shopsystem/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'shopsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER': 'shopsys',
+        'PASSWORD': '123456',     
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'Testing': {
+        }
     }
 }
 
